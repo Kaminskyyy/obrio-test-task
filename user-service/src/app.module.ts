5362da562c76631368e65732bf2db43552e24eae
Stanislav Malakhov <thehustaan@gmail.com>
2024-05-22 20:10:31 +0300

    Cleanup

--- user-service/src/app.module.ts
+++ user-service/src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
 import { RabbitmqModule } from './rabbitmq/rabbitmq.module';
@@ -19,7 +17,5 @@ import rabbitmqConfig from './config/rabbitmq.config';
     RabbitmqModule,
     DatabaseModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
